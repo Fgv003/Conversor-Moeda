da308fbfb8e586101657d3c1a9e63d860b18289d
Fábio Vieira <fabio.gv003@gmail.com>
2024-06-24 20:51:04 -0300

    Consumo da Api

---
+++ src/main/java/com/conversorMoeda/Service/ConverteDados.java
@@ -0,0 +1,19 @@
+package com.conversorMoeda.Service;
+
+
+import com.google.gson.Gson;
+
+public class ConverteDados implements IConverteDados {
+    private Gson gson = new Gson();
+
+    @Override
+    public <T> T obterDados(String json, Class<T> classe) {
+        try {
+            return gson.fromJson(json,classe);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
+
+//GET https://v6.exchangerate-api.com/v6/YOUR-API-KEY/pair/EUR/GBP/AMOUNT
