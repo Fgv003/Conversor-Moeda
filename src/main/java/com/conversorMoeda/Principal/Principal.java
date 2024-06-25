2b45a1730a9a461294d35661ba0629de9b532fcc
Fábio Vieira <fabio.gv003@gmail.com>
2024-06-25 13:08:19 -0300

    Correções menu

--- src/main/java/com/conversorMoeda/Principal/Principal.java
+++ src/main/java/com/conversorMoeda/Principal/Principal.java
@@ -10,13 +10,9 @@ import java.util.Scanner;
 
 public class Principal {
 
-    //https://v6.exchangerate-api.com/v6/a2a94f2799edbd821c0e01d0/pair/EUR/GBP/AMOUNT
-
     Scanner sc = new Scanner(System.in);
     ConsumoApi consumoApi = new ConsumoApi();
     ConverteDados converteDados = new ConverteDados();
-
-    private final String ENDERECO = "https://v6.exchangerate-api.com/v6/a2a94f2799edbd821c0e01d0/pair/";
 
     List<ConversorMoeda> moedasConvertidas = new ArrayList<>();
 
@@ -42,58 +38,57 @@ public class Principal {
             System.out.println("********************************************");
             opc = sc.nextInt();
 
-
-
-            if (opc != 10 || opc != 9) {
+            if (opc <=8) {
                 System.out.println("Qual valor gostaria de converter ?");
                 valor = sc.nextDouble();
             }
 
+            String endereco = "https://v6.exchangerate-api.com/v6/a2a94f2799edbd821c0e01d0/pair/";
             switch (opc){
                 case 1:
-                    var c1 = consumoApi.obterDados(ENDERECO + "USD/ARS/" + valor);
+                    var c1 = consumoApi.obterDados(endereco + "USD/ARS/" + valor);
                     ConversorMoeda dolarPeso = converteDados.obterDados(c1, ConversorMoeda.class);
                     moedasConvertidas.add(dolarPeso);
                     System.out.println("O valor " + valor + " [USD] corresponde ao valor final de " + dolarPeso.resultadoConversao() + " [ARS]");
                     break;
                 case 2:
-                    var c2 = consumoApi.obterDados(ENDERECO + "ARS/USD/" + valor);
+                    var c2 = consumoApi.obterDados(endereco + "ARS/USD/" + valor);
                     ConversorMoeda pesoDolar = converteDados.obterDados(c2, ConversorMoeda.class);
                     moedasConvertidas.add(pesoDolar);
                     System.out.println("O valor " + valor + " [ARS] corresponde ao valor final de " + pesoDolar.resultadoConversao() + " [USD]");
                     break;
                 case 3:
-                    var c3 = consumoApi.obterDados(ENDERECO + "USD/BRL/" + valor);
+                    var c3 = consumoApi.obterDados(endereco + "USD/BRL/" + valor);
                     ConversorMoeda dolarReal = converteDados.obterDados(c3, ConversorMoeda.class);
                     moedasConvertidas.add(dolarReal);
                     System.out.println("O valor " + valor + " [USD] corresponde ao valor final de " + dolarReal.resultadoConversao() + " [BRL]");
                     break;
                 case 4:
-                    var c4 = consumoApi.obterDados(ENDERECO + "BRL/USD/" + valor);
+                    var c4 = consumoApi.obterDados(endereco + "BRL/USD/" + valor);
                     ConversorMoeda realDolar = converteDados.obterDados(c4, ConversorMoeda.class);
                     moedasConvertidas.add(realDolar);
                     System.out.println("O valor " + valor + " [BRL] corresponde ao valor final de " + realDolar.resultadoConversao() + " [USD]");
                     break;
                 case 5:
-                    var c5 = consumoApi.obterDados(ENDERECO + "USD/EUR/" + valor);
+                    var c5 = consumoApi.obterDados(endereco + "USD/EUR/" + valor);
                     ConversorMoeda dolarEuro = converteDados.obterDados(c5, ConversorMoeda.class);
                     moedasConvertidas.add(dolarEuro);
                     System.out.println("O valor " + valor + " [USD] corresponde ao valor final de " + dolarEuro.resultadoConversao() + " [EUR]");
                     break;
                 case 6:
-                    var c6 = consumoApi.obterDados(ENDERECO + "EUR/USD/" + valor);
+                    var c6 = consumoApi.obterDados(endereco + "EUR/USD/" + valor);
                     ConversorMoeda euroDolar = converteDados.obterDados(c6, ConversorMoeda.class);
                     moedasConvertidas.add(euroDolar);
                     System.out.println("O valor " + valor + " [EUR] corresponde ao valor final de " + euroDolar.resultadoConversao() + " [USD]");
                     break;
                 case 7:
-                    var c7 = consumoApi.obterDados(ENDERECO + "EUR/BRL/" + valor);
+                    var c7 = consumoApi.obterDados(endereco + "EUR/BRL/" + valor);
                     ConversorMoeda euroReal = converteDados.obterDados(c7, ConversorMoeda.class);
                     moedasConvertidas.add(euroReal);
                     System.out.println("O valor " + valor + " [EUR] corresponde ao valor final de " + euroReal.resultadoConversao() + " [BRL]");
                     break;
                 case 8:
-                    var c8 = consumoApi.obterDados(ENDERECO + "BRL/EUR/" + valor);
+                    var c8 = consumoApi.obterDados(endereco + "BRL/EUR/" + valor);
                     ConversorMoeda realEuro = converteDados.obterDados(c8, ConversorMoeda.class);
                     moedasConvertidas.add(realEuro);
                     System.out.println("O valor " + valor + " [BRL] corresponde ao valor final de " + realEuro.resultadoConversao() + " [EUR]");
@@ -101,11 +96,13 @@ public class Principal {
                 case 9:
                     System.out.println("Últimas conversões");
                     System.out.println(moedasConvertidas.toString());
+                    break;
+                case 10:
+                    System.out.println("Até mais :)");
+                    break;
                 default:
                     System.out.println("Opção inválida - Digite novamente");
             }
-
         }while (opc != 10);
-
     }
 }
--- src/main/java/com/conversorMoeda/Service/ConverteDados.java
+++ src/main/java/com/conversorMoeda/Service/ConverteDados.java
@@ -15,8 +15,3 @@ public class ConverteDados implements IConverteDados {
         }
     }
 }
-
-//GET https://v6.exchangerate-api.com/v6/YOUR-API-KEY/pair/EUR/GBP/AMOUNT
-
-//https://v6.exchangerate-api.com/v6/a2a94f2799edbd821c0e01d0/latest/USD
-
